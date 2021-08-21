38537600430e8e2f0b3009666f6711eeba86d18d
didactic-goggles <58259532+developerd1320@users.noreply.github.com>
2021-08-21 16:37:23 +0300

    test aws

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
-app.use('/', indexRouter);
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './build', 'index.html'))
+});
 
 module.exports = app;
